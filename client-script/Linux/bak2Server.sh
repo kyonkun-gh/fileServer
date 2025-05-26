04e8afe9e2f96c52b27216ff070a9aa82f38f386
kyonkun-gh <kyonkun@cht.com.tw>
2025-05-26 18:24:23 +0800

    Move configure to my.conf Load data from my.conf

--- client-script/Linux/bak2Server.sh
+++ client-script/Linux/bak2Server.sh
@@ -1,71 +1,82 @@
 #!/bin/bash
 
-#Color
-COLOR_RESET=$'\e[0m'
-COLOR_RED=$'\e[0;31m'
-COLOR_GREEN=$'\e[0;32m'
-COLOR_YELLOW=$'\e[0;33m'
-
 #Static Variable
-protocol="http://"
-host="127.0.0.1"
-port="8080"
 txtFilePath="upload2ServerFile.txt"
 
 #Dynamic Variable
 shellPath=$(dirname $0)
-url="$protocol$host:$port/"
-curMonth=`date --date="$(date +%Y-%m-15) -1 month" "+%m"`
+
+#Load conf data
+source ${shellPath}/my.conf
+url="${PROTOCOL}${REMOTE_HOST}:${PORT}/"
+lastMonth=`date --date="$(date +%Y-%m-15) -1 month" "+%m"`
 filePaths=()
+
 errorCount=0
+if [ -z ${PROTOCOL} ]; then
+	echo -e "${COLOR_ERROR}PROTOCOL not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ -z ${REMOTE_HOST} ]; then
+	echo -e "${COLOR_ERROR}REMOTE_HOST not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ -z ${PORT} ]; then
+	echo -e "${COLOR_ERROR}PORT not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ ${errorCount} -gt 0 ]; then
+	echo -e "${COLOR_ERROR}Configure has some error, please check!${COLOR_RESET}"
+	exit 1
+fi
 
 #Change Directory
 cd $shellPath
 
 
-nc -z ${host} ${port}
+nc -z ${REMOTE_HOST} ${PORT}
 if [ ! $? -eq 0 ]; then
-	echo -e "$url${COLOR_RED} is not open. EXIT ${COLOR_RESET}"
+	echo -e "${COLOR_ERROR}${url}${COLOR_RESET} is not open, please check!"
 	exit 1
 fi
 
-if [ ! -f $txtFilePath ]; then
-	echo -e "$txtFilePath${COLOR_RED} does not exists. EXIT ${COLOR_RESET}"
+if [ ! -f ${txtFilePath} ]; then
+	echo -e "${COLOR_ERROR}${txtFilePath}${COLOR_RESET} does not exists, please check!"
 	exit 1
 fi
 
 while IPS= read -r line
 do
-	filePath=`echo $line | sed -e "s/{MONTH}/${curMonth}/"`
+	filePath=`echo $line | sed -e "s/{MONTH}/${lastMonth}/"`
 	filePaths+=("$filePath")
-	if [ ! -f $filePath ]; then
+	if [ ! -f ${filePath} ]; then
 		errorCount=$((errorCount+1))
-		echo -e "UPLOAD FILE NOT FOUND: ${COLOR_RED}$filePath ${COLOR_RESET}"
+		echo -e "UPLOAD FILE NOT FOUND: ${COLOR_ERROR}${filePath}${COLOR_RESET}"
 	fi
-done < $txtFilePath
+done < ${txtFilePath}
 
-if [ $errorCount -gt 0 ]; then
-	echo -e "${COLOR_RED}Some target files do not exists. EXIT${COLOR_RESET}"
+if [ ${errorCount} -gt 0 ]; then
+	echo -e "${COLOR_ERROR}Some target files do not exists, please check!${COLOR_RESET}"
 	exit 1
 fi
 
 
 for filePath in ${filePaths[@]}; do
-	data=`ls -l $filePath`
-	echo -e "${COLOR_GREEN}$data${COLOR_RESET}"
+	data=`ls -l ${filePath}`
+	echo -e "${COLOR_INFO}${data}${COLOR_RESET}"
 done
 
-read -p "${COLOR_YELLOW}Do you want to upload above file(s)? (y)Yes/(n)No: ${COLOR_RESET}" choice
+read -p "${COLOR_MSG}Do you want to upload above file(s)? (y)Yes/(n)No: ${COLOR_RESET}" choice
 
-case $choice in
+case ${choice} in
 	[yY]* )	;;
-	*)	echo -e "${COLOR_RED}Script leave!!${COLOR_RESET}"
+	*)	echo -e "${COLOR_ERROR}Script leave!!${COLOR_RESET}"
 		exit 0 ;;
 esac
 
 
 for filePath in ${filePaths[@]}; do
-	echo -e "FILE: ${COLOR_GREEN}$filePath ${COLOR_RESET}Uploading..."
-	curl -v -F file=@$filePath $url
+	echo -e "FILE: ${COLOR_INFO}${filePath}${COLOR_RESET} Uploading..."
+	curl -v -F file=@${filePath} ${url}
 done
 
--- client-script/Linux/dir2Server.sh
+++ client-script/Linux/dir2Server.sh
@@ -1,42 +1,61 @@
 #!/bin/bash
 
-#Color
-COLOR_RESET=$'\e[0m'
-COLOR_RED=$'\e[0;31m'
-COLOR_GREEN=$'\e[0;32m'
-
-#Static Variable
-protocol="http://"
-host="127.0.0.1"
-port="8080"
-
 #Dynamic Variable
 shellPath=$(dirname $0)
-url="$protocol$host:$port/"
-defaultPath="/root/Downloads"
+
+#Load conf data
+source ${shellPath}/my.conf
+url="${PROTOCOL}${REMOTE_HOST}:${PORT}/"
 path=$1
 
+errorCount=0
+if [ -z ${PROTOCOL} ]; then
+	echo -e "${COLOR_ERROR}PROTOCOL not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ -z ${REMOTE_HOST} ]; then
+	echo -e "${COLOR_ERROR}REMOTE_HOST not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ -z ${PORT} ]; then
+	echo -e "${COLOR_ERROR}PORT not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ -z ${DEFAULT_UPLOAD_PATH} ]; then
+	echo -e "${COLOR_ERROR}DEFAULT_UPLOAD_PATH not set, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+elif [ ! -d ${DEFAULT_UPLOAD_PATH} ]; then
+	echo -e "${COLOR_ERROR}${DEFAULT_UPLOAD_PATH} is not a directory, please check!${COLOR_RESET}"
+	errorCount=$((errorCount+1))
+fi
+if [ ${errorCount} -gt 0 ]; then
+	echo -e "${COLOR_ERROR}Configure has some error, please check!${COLOR_RESET}"
+	exit 1
+fi
 
-if [ "X${path}" == "X" ]; then
-	echo -e "${COLOR_GREEN}Use Default Directory ${defaultPath}${COLOR_RESET}"
-	path=${defaultPath}
+
+if [ -z "${path}" ]; then
+	echo -e "Use Default Directory: ${COLOR_INFO}${DEFAULT_UPLOAD_PATH}${COLOR_RESET}"
+	path=${DEFAULT_UPLOAD_PATH}
 elif [ ! -d ${path} ]; then
-	echo -e "${COLOR_RED}Not Directory, Use Default Directory ${defaultPath}${COLOR_RESET}"
-	path=${defaultPath}
+	echo -e "${COLOR_ERROR}${path}${COLOR_RESET} is not a directory. Use Default Directory: ${COLOR_INFO}${DEFAULT_UPLOAD_PATH}${COLOR_RESET}"
+	path=${DEFAULT_UPLOAD_PATH}
 else
 	path=`realpath ${path}`
 fi
 
-nc -z ${host} ${port}
+
+nc -z ${REMOTE_HOST} ${PORT}
 if [ ! $? -eq 0 ]; then
-	echo -e "${url}${COLOR_RED} is not open. EXIT ${COLOR_RESET}"
+	echo -e "${COLOR_ERROR}${url}${COLOR_RESET} is not open, please check!"
 	exit 1
 fi
 
+
 for filePath in ${path}/*; do
-	if [ -f $filePath ]; then
-		echo -e "FILE: ${COLOR_GREEN}$filePath ${COLOR_RESET}Uploading..."
-		curl -v -F file=@$filePath $url
+	if [ -f ${filePath} ]; then
+		echo -e "FILE: ${COLOR_INFO}${filePath}${COLOR_RESET} Uploading..."
+		curl -v -F file=@${filePath} ${url}
 	fi
 done
 
